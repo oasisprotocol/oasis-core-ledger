785922d19c0b707832e912dae8832bfff874e18d
Juan Leni <juan.leni@zondax.ch>
2020-01-22 22:53:50 +0100

    updating testcases

--- app_test.go
+++ app_test.go
@@ -56,8 +56,7 @@ func Test_UserGetVersion(t *testing.T) {
 
 	assert.Equal(t, uint8(0x0), version.AppMode, "TESTING MODE ENABLED!!")
 	assert.Equal(t, uint8(0x0), version.Major, "Wrong Major version")
-	assert.Equal(t, uint8(0x8), version.Minor, "Wrong Minor version")
-	assert.Equal(t, uint8(0x0), version.Patch, "Wrong Patch version")
+	assert.Equal(t, uint8(0xd), version.Minor, "Wrong Minor version")
 }
 
 func Test_UserGetPublicKey(t *testing.T) {
@@ -69,7 +68,7 @@ func Test_UserGetPublicKey(t *testing.T) {
 
 	app.api.Logging = true
 
-	path := []uint32{44, 118, 5, 0, 21}
+	path := []uint32{44, 474, 5, 0, 21}
 
 	pubKey, err := app.GetPublicKeyEd25519(path)
 	if err != nil {
@@ -81,7 +80,7 @@ func Test_UserGetPublicKey(t *testing.T) {
 	fmt.Printf("PUBLIC KEY: %x\n", pubKey)
 
 	assert.Equal(t,
-		"3fbe6fd4cba729c23400510bbc49f90ec76d44204e7e921795e764e6a51fe387",
+		"d71c79ffd5a6d438de89c833e00222a2e80ed94e9929350ef7c1c97d1d13295d",
 		hex.EncodeToString(pubKey),
 		"Unexpected pubkey")
 }
@@ -95,7 +94,7 @@ func Test_GetAddressPubKeyEd25519_Zero(t *testing.T) {
 
 	app.api.Logging = true
 
-	path := []uint32{44, 118, 0, 0, 0}
+	path := []uint32{44, 474, 0, 0, 0}
 
 	pubKey, addr, err := app.GetAddressPubKeyEd25519(path)
 	if err != nil {
@@ -107,8 +106,8 @@ func Test_GetAddressPubKeyEd25519_Zero(t *testing.T) {
 
 	assert.Equal(t, 32, len(pubKey), "Public key has wrong length: %x, expected length: %x\n", pubKey, 32)
 
-	assert.Equal(t, "2dc2ba9143c81c44830da47b0962ed026ba3e2e3d7a24d4a6ff1a418e9d154be", hex.EncodeToString(pubKey), "Unexpected pubkey")
-	assert.Equal(t, "oasis19hpt4y2reqwyfqcd53asjchdqf468chr673y6jn07xjp36w32jlscf0me", addr, "Unexpected addr")
+	assert.Equal(t, "97e72e6e83ec39eb98d7e9189513aba662a08a210b9974b0f7197458483c7161", hex.EncodeToString(pubKey), "Unexpected pubkey")
+	assert.Equal(t, "oasis1jlnjum5rasu7hxxhayvf2yat5e32pz3ppwvhfv8hr969sjpuw9sgn54g9", addr, "Unexpected addr")
 }
 
 func Test_GetAddressPubKeyEd25519(t *testing.T) {
@@ -120,7 +119,7 @@ func Test_GetAddressPubKeyEd25519(t *testing.T) {
 
 	app.api.Logging = true
 
-	path := []uint32{44, 118, 5, 0, 21}
+	path := []uint32{44, 474, 5, 0, 21}
 
 	pubKey, addr, err := app.GetAddressPubKeyEd25519(path)
 	if err != nil {
@@ -132,8 +131,8 @@ func Test_GetAddressPubKeyEd25519(t *testing.T) {
 
 	assert.Equal(t, 32, len(pubKey), "Public key has wrong length: %x, expected length: %x\n", pubKey, 32)
 
-	assert.Equal(t, "3fbe6fd4cba729c23400510bbc49f90ec76d44204e7e921795e764e6a51fe387", hex.EncodeToString(pubKey), "Unexpected pubkey")
-	assert.Equal(t, "oasis187lxl4xt5u5uydqq2y9mcj0epmrk63pqfelfy9u4uajwdfgluwrk0e5vx", addr, "Unexpected addr")
+	assert.Equal(t, "d71c79ffd5a6d438de89c833e00222a2e80ed94e9929350ef7c1c97d1d13295d", hex.EncodeToString(pubKey), "Unexpected pubkey")
+	assert.Equal(t, "oasis16uw8nl745m2r3h5feqe7qq3z5t5qak2wny5n2rhhc8yh68gn99wwcq4ef", addr, "Unexpected addr")
 }
 
 func Test_ShowAddressPubKeyEd25519(t *testing.T) {
@@ -145,7 +144,7 @@ func Test_ShowAddressPubKeyEd25519(t *testing.T) {
 
 	app.api.Logging = true
 
-	path := []uint32{44, 118, 5, 0, 21}
+	path := []uint32{44, 474, 5, 0, 21}
 
 	pubKey, addr, err := app.ShowAddressPubKeyEd25519(path)
 	if err != nil {
@@ -157,8 +156,8 @@ func Test_ShowAddressPubKeyEd25519(t *testing.T) {
 
 	assert.Equal(t, 32, len(pubKey), "Public key has wrong length: %x, expected length: %x\n", pubKey, 32)
 
-	assert.Equal(t, "3fbe6fd4cba729c23400510bbc49f90ec76d44204e7e921795e764e6a51fe387", hex.EncodeToString(pubKey), "Unexpected pubkey")
-	assert.Equal(t, "oasis187lxl4xt5u5uydqq2y9mcj0epmrk63pqfelfy9u4uajwdfgluwrk0e5vx", addr, "Unexpected addr")
+	assert.Equal(t, "d71c79ffd5a6d438de89c833e00222a2e80ed94e9929350ef7c1c97d1d13295d", hex.EncodeToString(pubKey), "Unexpected pubkey")
+	assert.Equal(t, "oasis16uw8nl745m2r3h5feqe7qq3z5t5qak2wny5n2rhhc8yh68gn99wwcq4ef", addr, "Unexpected addr")
 }
 
 func Test_UserPK_HDPaths(t *testing.T) {
@@ -170,19 +169,19 @@ func Test_UserPK_HDPaths(t *testing.T) {
 
 	app.api.Logging = true
 
-	path := []uint32{44, 118, 0, 0, 0}
+	path := []uint32{44, 474, 0, 0, 0}
 
 	expected := []string{
-		"2dc2ba9143c81c44830da47b0962ed026ba3e2e3d7a24d4a6ff1a418e9d154be",
-		"660e4d231f86a24549cae9e0e57879379fb33306374d0afbfee17075df569792",
-		"2d5dfd35f26079c8b45cdb028ddd0d1d3c9a49a6e67935def053a0019528a357",
-		"be01ac0e3b210c0488bb3e2ade554805f9fb92a2b6f2b3124a9d3055bd899432",
-		"b31c272d30133ddd3750efaf631b7cf7f69724b9f9e345f1881017a288e19081",
-		"658c6217e868b05bc221cd9fc7ec1720b156a3e7def0181d9bf772d4c988c625",
-		"0efa4ee32aa2b0570fee44a1c9890fbf49c841f190d11be4bbe78a29ec21ee0d",
-		"addc2e6cfe74b887d36a184a7c530846cc506101f7948afea1803df49ff844cb",
-		"08d2c564382a9b2d9ed481bac0f23e98a24b6f685fb46f01b1388aa7735b22c7",
-		"540c266ce814964a652c9dccd23377b8f9adc157b6b3c8269838fcd2223c875d",
+		"97e72e6e83ec39eb98d7e9189513aba662a08a210b9974b0f7197458483c7161",
+		"54e98ea8afcf1321eddd2c91ee71f7f9237c38bd8c3242057be5c7ce3f46abbd",
+		"7d10a11e1a4ef5adea33eb9f3332c6d221c12d461299de32d10e6cfffcd776d8",
+		"00f3a005092933e8c2956d7ece62cbd39718678e35bf2a7370c344e9e755bc18",
+		"3c713b1b2623c3a1c997b7b80c9dce4c49bf32c36dabb5cea6ce2cb6e89eb600",
+		"636586ccbca4c1a5035552faccbce3b6ca59e6181ce17a3d84bcf6d9c5d120d1",
+		"887fca7f936cad2733c6c8100c2ca8c612a37b9c7645b4a4b58445e5ceb6e862",
+		"e2c22521953488a0135a4348dfd7544ff8ecfa1744fda1bef2f935476b909115",
+		"5fec8d7031821c0a7ebbc18bdcaad826e1cf83323e172ce0a4f36a8e04792696",
+		"72fde11509927324be809cdc815b258678ea74b2aa1d5e5490a960acd86c7a7e",
 	}
 
 	for i := uint32(0); i < 10; i++ {
@@ -203,7 +202,7 @@ func Test_UserPK_HDPaths(t *testing.T) {
 			t,
 			expected[i],
 			hex.EncodeToString(pubKey),
-			"Public key 44'/118'/0'/0/%d does not match\n", i)
+			"Public key 44'/474'/0'/0/%d does not match\n", i)
 	}
 }
 
@@ -223,7 +222,7 @@ func Test_Sign(t *testing.T) {
 
 	app.api.Logging = true
 
-	path := []uint32{44, 118, 0, 0, 5}
+	path := []uint32{44, 474, 0, 0, 5}
 
 	message := getDummyTx()
 	signature, err := app.SignEd25519(path, []byte(coinContext), message)
@@ -261,7 +260,7 @@ func Test_Sign_Fails(t *testing.T) {
 
 	app.api.Logging = true
 
-	path := []uint32{44, 118, 0, 0, 5}
+	path := []uint32{44, 474, 0, 0, 5}
 
 	message := getDummyTx()
 	garbage := []byte{65}
@@ -279,6 +278,6 @@ func Test_Sign_Fails(t *testing.T) {
 	_, err = app.SignEd25519(path, []byte(coinContext), message)
 	assert.Error(t, err)
 	errMessage = err.Error()
-	assert.Equal(t, errMessage, "Unexpected data at end")
+	assert.Equal(t, errMessage, "Unexpected CBOR EOF")
 
 }
